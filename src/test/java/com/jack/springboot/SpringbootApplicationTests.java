1ac89dc6c4ac3c0b8a3feb762c6d1bf6e071b072
Bradly-s <3308688609@qq.com>
2022-07-31 19:47:38 +0800

    springboot+junit+mybatis_plus

--- src/test/java/com/jack/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/jack/springboot/SpringbootApplicationTests.java
@@ -1,25 +1,41 @@
 package com.jack.springboot;
 
-import com.jack.springboot.service.ICargoService;
-import com.jack.springboot.service.impl.CargoServiceImpl;
+import com.jack.springboot.dao.CargoDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+
     @Autowired
-    private ICargoService cargoService;
+    private CargoDao cargoDao;
 
     @Test
     public void testFindById(){
-        System.out.println(cargoService.findById(4));
+        System.out.println(cargoDao.selectById(4));
     }
 
     @Test
     public void testFindAll(){
-        System.out.println(cargoService.findAll());
+        System.out.println(cargoDao.selectList(null));
     }
+
+
+//--------springboot+junit+mybatis
+
+//    @Autowired
+//    private ICargoService cargoService;
+//
+//    @Test
+//    public void testFindById(){
+//        System.out.println(cargoService.findById(4));
+//    }
+//
+//    @Test
+//    public void testFindAll(){
+//        System.out.println(cargoService.findAll());
+//    }
 
 }
 
